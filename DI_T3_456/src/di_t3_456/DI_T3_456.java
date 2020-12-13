a0da301ca4d666bd8906eb6419a538261e459388
IESMontecillosMarcos <71645845+IESMontecillosMarcos@users.noreply.github.com>
2020-12-13 20:41:01 +0100

    Add files via upload

---
+++ DI_T3_456/src/di_t3_456/DI_T3_456.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package di_t3_456;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Marcos Gonzalez Leon
+ */
+public class DI_T3_456 extends Application {
+    
+    final static Label label = new Label();
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 150);
+        stage.setScene(scene);
+        stage.setTitle("Text Field Sample");
+ 
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+ 
+        scene.setRoot(grid);
+        final Label dollar = new Label("$");
+        GridPane.setConstraints(dollar, 0, 0);
+        grid.getChildren().add(dollar);
+        
+        final TextField sum = new TextField() {
+            @Override
+            public void replaceText(int start, int end, String text) {
+                if (!text.matches("[a-z, A-Z]")) {
+                    super.replaceText(start, end, text);   
+                }
+                label.setText("Enter a numeric value");
+            }
+ 
+            @Override
+            public void replaceSelection(String text) {
+                if (!text.matches("[a-z, A-Z]")) {
+                    super.replaceSelection(text);
+                }
+            }
+        };
+ 
+        sum.setPrefColumnCount(10);
+        GridPane.setConstraints(sum, 1, 0);
+        grid.getChildren().add(sum);
+        
+        Button submit = new Button("Submit");
+        GridPane.setConstraints(submit, 2, 0);
+        grid.getChildren().add(submit);
+        
+        submit.setOnAction((ActionEvent e) -> {
+            label.setText(null);
+        });
+        
+        GridPane.setConstraints(label, 0, 1);
+        GridPane.setColumnSpan(label, 3);
+        grid.getChildren().add(label);
+        
+        scene.setRoot(grid);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
